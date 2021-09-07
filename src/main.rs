2560f1c1aaf351a720960afae50f98ed8890a267
Mikhail Volkhov <volhovm.cs@gmail.com>
2021-09-07 18:50:45 +0300

    Toy verification of a zk-paillier proof

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use curv::BigInt;
+use paillier::*;
+use zk_paillier::zkproofs::{CiphertextProof,CiphertextWitness,CiphertextStatement};
+
+
+fn check_correct_ciphertext_proof() {
+    let kp:Keypair = Paillier::keypair();
+    let (pk,_) = kp.keys();
+
+    let m = BigInt::from(1u32);
+    let r = BigInt::from(2u32);
+
+    let ct = Paillier::encrypt_with_chosen_randomness(
+             &pk,
+             RawPlaintext::from(m.clone()),
+             &Randomness(r.clone())).0.into_owned();
+
+    let w = CiphertextWitness{ x: m.clone(), r: r };
+    let w_false = CiphertextWitness{x:m,r:BigInt::from(3)};
+    let x = CiphertextStatement{ ek: pk, c: ct };
+
+    let res1 = CiphertextProof::prove(&w,&x).verify(&x);
+    println!("Honest proof verifies: {:?}", res1);
+
+    let res2 = CiphertextProof::prove(&w_false,&x).verify(&x);
+    println!("Bogus proof verification gives: {:?}", res2);
+}
+
 fn main() {
-    println!("Hello, world!");
+    check_correct_ciphertext_proof();
 }
